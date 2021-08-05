e78c7af1f8613d58087d2e0378918797b5826d14
asmithasatesh <asmithasatesh@gmail.com>
2021-08-05 11:46:49 +0530

    [Asmitha]Initial commit with welcome Message

---
+++ LINQ_AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LINQ AddressBookSystem!");
+        }
+    }
+}
