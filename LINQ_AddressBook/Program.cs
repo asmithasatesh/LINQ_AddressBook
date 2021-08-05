5c3ba50e1f497587461c31c9f652cf74f0e0c3a2
asmithasatesh <asmithasatesh@gmail.com>
2021-08-05 13:07:16 +0530

    [Asmitha]Insert values in DataTable

--- LINQ_AddressBook/Program.cs
+++ LINQ_AddressBook/Program.cs
@@ -7,6 +7,34 @@ namespace LINQ_AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LINQ AddressBookSystem!");
+            //Create Object for DataTable
+            ContactDataManager contactDataManager = new ContactDataManager();
+            ContactDataManager contactDataManagers = new ContactDataManager();
+            DataTableManger dataTableManger = new DataTableManger();
+            dataTableManger.CreateDataTable();
+
+            //Insert Values into Table
+            contactDataManager.FirstName = "Ash";
+            contactDataManager.LastName = "Satesh";
+            contactDataManager.PhoneNumber = 9842905050;
+            contactDataManager.Email = "asmitha@gmail.com";
+            contactDataManager.Address = "139 B PB marg";
+            contactDataManager.City = "Bareilly";
+            contactDataManager.State = "UP";
+            contactDataManager.zip = 243001;
+            dataTableManger.InsertintoDataTable(contactDataManager);
+
+            //Insert Values into Table
+            contactDataManagers.FirstName = "lalita";
+            contactDataManagers.LastName = "poojah";
+            contactDataManagers.PhoneNumber = 7742905050;
+            contactDataManagers.Email = "lalita@gmail.com";
+            contactDataManagers.Address = "Catherine Street B PB marg";
+            contactDataManagers.City = "Bareilly";
+            contactDataManagers.State = "UP";
+            contactDataManagers.zip = 123001;
+            dataTableManger.InsertintoDataTable(contactDataManagers);
+            dataTableManger.Display();
         }
     }
 }
